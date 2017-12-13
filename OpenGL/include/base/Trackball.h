f821c62ee8a6f3dfe6afc9212cab2b4f1c8ed866
knight <xtknightMM@iCloud.com>
2017-12-13 21:56:43 +0800

    begin trackball

---
+++ OpenGL/include/base/Trackball.h
@@ -0,0 +1,13 @@
+//
+//  Trackball.h
+//  openGLTest
+//
+//  Created by xt on 17/12/13.
+//  Copyright © 2017年 xt. All rights reserved.
+//
+
+#ifndef Trackball_h
+#define Trackball_h
+
+
+#endif /* Trackball_h */
---
+++ OpenGL/src/base/Trackball.cpp
@@ -0,0 +1,9 @@
+//
+//  Trackball.cpp
+//  openGLTest
+//
+//  Created by xt on 17/12/13.
+//  Copyright © 2017年 xt. All rights reserved.
+//
+
+#include <stdio.h>
